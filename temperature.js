3052739505455df52aa6bb37d64504d0deb600c4
alu0100814651 <alu0100814651@ull.edu.es>
2016-02-29 19:31:25 +0000

    commit
    añadiendo fichero css y modificando .html y .js

--- temperature.js
+++ temperature.js
@@ -2,20 +2,21 @@
 function calculate() {
   var result;
   var temp = original.value;
-  var regexp = /([-+]?\d+(?:\.\d*)?)\s*([fFcC])/;
-  
+  var regexp = /^[?:\s*]?\s*([-+]?\d+(?:\.\d*)?)\s*[e]?\s*([+-]?\d*)\s*([Cc](?:Celsius)?|[Ff](?:a|ar|ahrenheit)?)\b/;
+
   var m = temp.match(regexp);
-  
+
   if (m) {
     var num = m[1];
-    var type = m[2];
+    var ep = m[2];
+    var type = m[3];
     num = parseFloat(num);
     if (type == 'c' || type == 'C') {
-      result = (num * 9/5)+32;
+      result = (num * Math.pow(10,ep) * 9/5)+32;
       result = result.toFixed(1)+" Farenheit"
     }
     else {
-      result = (num - 32)*5/9;
+      result = (num * Math.pow(10,ep) - 32)*5/9;
       result = result.toFixed(1)+" Celsius"
     }
     converted.innerHTML = result;
